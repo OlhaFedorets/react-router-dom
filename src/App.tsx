8c45d29bf0d5a44b66b53ccb3364ca510775831a
OlhaFedorets <fedoretsolha@gmail.com>
2025-02-04 17:33:32 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
+import {S} from "./components/pages/_styles";
+
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    ERROR: '/error404'
+} as const
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const finalClassName = () => {
+    //     ({isActive}) => isActive ? styles.activeNavLink : styles.navLink
+    // }
+
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    {/*вариант с помощью module*/}
+                    <div><NavLink to={PATH.PAGE1} className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>Page 1</NavLink></div>
+                    <div><NavLink to={PATH.PAGE2} className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>Page 2</NavLink></div>
+                    <div><NavLink to={PATH.PAGE3} className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink}>Page 3</NavLink></div>
+
+                    {/*вариант с помощью styled components*/}
+                    {/*<S.NavWrapper><NavLink to={PATH.PAGE1}>Page 1</NavLink></S.NavWrapper>*/}
+                    {/*<S.NavWrapper><NavLink to={PATH.PAGE2}>Page 2</NavLink></S.NavWrapper>*/}
+                    {/*<S.NavWrapper><NavLink to={PATH.PAGE3}>Page 3</NavLink></S.NavWrapper>*/}
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to={PATH.PAGE1}/>}/>
+
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
+
+                        <Route path={PATH.ERROR} element={<Error404/>}/>
+                        <Route path="/*" element={<Navigate to={PATH.ERROR}/>}/>
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
+        </div>
+    );
 }
+
+
+
 
 export default App;
