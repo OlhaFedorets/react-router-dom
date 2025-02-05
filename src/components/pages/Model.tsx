8afa0f1c081a1b2ab315e1432a4c5666034cd198
OlhaFedorets <fedoretsolha@gmail.com>
2025-02-05 19:22:00 +0300

    change model

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import {adidasArr} from './Adidas'
 import {useParams} from "react-router-dom";
+import {Error404} from "./Error404";
 
 
 export const Model = () => {
     const params = useParams();
     console.log(params)
-    const id: number =  adidasArr.findIndex(adidas=>adidas.id === Number(params.id))
+    const id: number = adidasArr.findIndex(adidas => adidas.id === Number(params.id))
+    // const id = adidasArr.find(adidas=>adidas.id === Number(params.id)) // с помощью find
     return (
-        <div style={{textAlign:'center'}}>
-            <h2>{adidasArr[id].model}</h2>
-            <h3>{adidasArr[id].collection}</h3>
-            <h4>{adidasArr[id].price}</h4>
+        id !== -1 ?
+            <div style={{textAlign: 'center'}}>
+                <h2>{adidasArr[id].model}</h2>
+                <h3>{adidasArr[id].collection}</h3>
+                <h4>{adidasArr[id].price}</h4>
 
-            <img
-            src={adidasArr[id].picture}
-            alt={adidasArr[id].model}
-            style={{width: '600px', height: 'auto', marginRight: '10px'}}
-            />
-        </div>
-    );
+                <img
+                    src={adidasArr[id].picture}
+                    alt={adidasArr[id].model}
+                    style={{width: '600px', height: 'auto', marginRight: '10px'}}
+                />
+            </div>
+            : <Error404/>
+    )
 };
